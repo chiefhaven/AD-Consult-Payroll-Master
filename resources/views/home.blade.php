77471fc30e1c7c5a93407bbf1780ea9e8c6bbc0d
Chief Haven <chikumbahappy@gmail.com>
2024-04-20 00:45:50 +0200

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,11 +3,40 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+    <h1>&nbsp;</h1>
 @stop
 
 @section('content')
-    <p>Welcome to this beautiful admin panel.</p>
+    <div class="row">
+        <div class="col-lg-9">
+            <div class="row">
+                <div class="col-lg-8">
+                    <div class="card bg-primary mb-3 p-4">
+                        <div class="box-body">
+                            <h3>Good Morning</h3>
+                            Welcome to AD; Believe it possible, its all in your mind! Have a fresh day!.
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h3>08:00AM</h3>
+                            03/02/2024
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-3">
+            <div class="card">
+                <div class="card-body">
+                    <p>Upcoming events</p>
+                    Welcome to AD; Believe it possible, its all in your mind! Have a fresh day!.
+                </div>
+            </div>
+        </div>
+    </div>
 @stop
 
 @section('css')
