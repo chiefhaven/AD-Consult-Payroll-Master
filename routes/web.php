b5d8b53fd3b3fbe52d222f61ff6fe6f5baffd9f9
Chief Haven <chikumbahappy@gmail.com>
2024-04-21 16:08:37 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Livewire\Counter;
-use App\Livewire\CreatePost;
 
 Auth::routes();
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home')->middleware();
-
-Route::get('/counter', CreatePost::class);
