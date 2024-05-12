f0f5adfadc326f8a5d716386ea0e8bcae5a778ae
CleverSibale <108902673+CleverTheProgrammer@users.noreply.github.com>
2024-05-12 23:10:46 +0200

    Migrations

--- app/Models/HumanResourceAdmin.php
+++ app/Models/HumanResourceAdmin.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class HRAdmin extends Model
+class HumanResourceAdmin extends Model
 {
     use HasFactory;
 }
