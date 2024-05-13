2c6291f0cf7e47f5fbbb9b7a30f9d79570ce43d7
CleverSibale <108902673+CleverTheProgrammer@users.noreply.github.com>
2024-05-13 19:42:42 +0200

    migrations
    leave and admins

--- database/migrations/2024_04_28_114351_create_leaves_table.php
+++ database/migrations/2024_04_28_114351_create_leaves_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('leaves', function (Blueprint $table) {
             $table->id();
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->enum('type',['']);
             $table->timestamps();
         });
     }
