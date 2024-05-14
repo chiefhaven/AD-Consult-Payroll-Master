3c3b04d23501394212ce5766e284c586fce118e0
Chief Haven <chikumbahappy@gmail.com>
2024-05-15 00:10:05 +0200

    Update add-client.blade.php

--- resources/views/livewire/add-client.blade.php
+++ resources/views/livewire/add-client.blade.php
@@ -1,4 +1,5 @@
 <div class="col-lg-12">
+    @include('includes/error')
     @section('plugins.Select2', true)
     <form wire:submit="save">
         <div class="card mb-3 p-4">
@@ -6,28 +7,33 @@
                 <p>Client Details</p>
                 <div class="row">
                         <x-adminlte-input type="text" name="name" label="Name:*" placeholder="Name" fgroup-class="col-md-3"
-                        id="name" wire:model="name" required/>
+                        id="name" wire:model="form.name" required/>
 
                         <x-adminlte-input-file name="client_logo" label="Client logo" placeholder="Choose a file..."
                         fgroup-class="col-md-3"/>
 
                         <x-adminlte-input type="email" name="email" label="Email:*" placeholder="Email address" fgroup-class="col-md-3"
-                        id="email" wire:model="email" required/>
+                        id="email" wire:model="form.email" required/>
 
                         <x-adminlte-input type="tel" name="phone" label="Phone:*" placeholder="Phone" fgroup-class="col-md-3"
-                        id="phone" wire:model="phone" required/>
+                        id="phone" wire:model="form.phone" required/>
 
-                        <x-adminlte-select2 type="option" name="project" label="Project:" placeholder="Select an option..."
-                        fgroup-class="col-md-4">
-                            <option>UN Aids</option>
-                            <option>Cholera</option>
+                        <x-adminlte-input type="text" name="project" label="Project:" placeholder="Project" fgroup-class="col-md-3"
+                        id="project" wire:model="form.project"/>
+
+                        <x-adminlte-select2 type="option" name="industry" label="Industry:" wire:model="form.industry" placeholder="Select an option..."
+                        fgroup-class="col-md-3">
+                            <option>Health</option>
+                            <option>IT</option>
+                            <option>Telecomunication</option>
+                            <option>Education</option>
                         </x-adminlte-select2>
 
                         <x-adminlte-input type="date" name="contractstartdate" label="Contract start date:" placeholder="Contract start date:"
-                        fgroup-class="col-md-4" id="contractstartdate" wire:model="contractstartdate"/>
+                        fgroup-class="col-md-3" id="contractstartdate" wire:model="form.contractstartdate" required/>
 
                         <x-adminlte-input type="date" name="contractenddate" label="Contract end date:" placeholder="Contract end date"
-                        fgroup-class="col-md-4" id="contractenddate" wire:model="contractenddate"/>
+                        fgroup-class="col-md-3" id="contractenddate" wire:model="form.contractenddate"/>
 
                 </div>
             </div>
@@ -36,27 +42,30 @@
             <div class="box-body">
                 <p>More information</p>
                 <div class="row">
+                    <x-adminlte-input type="tel" name="phone2" label="Alternative Phone:*" placeholder="Alternative Phone" fgroup-class="col-md-3"
+                        id="phone2" wire:model="form.phone2"/>
+
                     <x-adminlte-input type="text" name="street_address" label="Street Adress:*"
                     placeholder="Street address" fgroup-class="col-md-3"
-                    id="street_address" wire:model="street_address" required/>
+                    id="street_address" wire:model="form.street_address"/>
 
                     <x-adminlte-input type="text" name="street_address_2" label="Street Adress 2:"
                     placeholder="Street address 2" fgroup-class="col-md-3"
-                    id="street_address_2" wire:model="street_address_2"/>
+                    id="street_address_2" wire:model="form.street_address_2"/>
 
                     <x-adminlte-input type="text" name="postal_code" label="Postal/Zip code:"
                     placeholder="Postal/Zip code" fgroup-class="col-md-3"
-                    id="postal_code" wire:model="postal_code"/>
+                    id="postal_code" wire:model="form.postal_code"/>
 
                     <x-adminlte-input type="text" name="city" label="City:*"
                     placeholder="City" fgroup-class="col-md-3"
-                    id="city" wire:model="city" required/>
+                    id="city" wire:model="form.city" required/>
 
                     <x-adminlte-input type="text" name="state" label="State/Region/Province:"
                     placeholder="State/Region/Province" fgroup-class="col-md-4"
-                     id="state" wire:model="state" required/>
+                     id="state" wire:model="form.state"/>
 
-                    <x-adminlte-select2 type="text" name="country" label="Country" wire:model="country" data-placeholder="Select an option..."
+                    <x-adminlte-select2 type="text" name="country" label="Country" wire:model="form.country" data-placeholder="Select an option..."
                         fgroup-class="col-md-4">
                         @foreach ($countries as $country)
                             <option {{ $country->name == 'Malawi' ? 'selected' : '' }}>{{$country->name}}</option>
@@ -65,19 +74,19 @@
 
                     <x-adminlte-input type="website" name="website" label="Website:"
                     placeholder="Website" fgroup-class="col-md-4"
-                     id="website" wire:model="website"/>
+                     id="website" wire:model="form.website"/>
 
                     <x-adminlte-input type="text" name="tax_number" label="Tax number:"
-                    placeholder="Tax number" fgroup-class="col-md-3" id="tax_number" wire:model="tax_number"/>
+                    placeholder="Tax number" fgroup-class="col-md-3" id="tax_number" wire:model="form.tax_number"/>
 
                     <x-adminlte-input type="text" name="tax_label" label="Tax label:"
-                    placeholder="Tax label" fgroup-class="col-md-3" id="tax_label" wire:model="tax_label"/>
+                    placeholder="Tax label" fgroup-class="col-md-3" id="tax_label" wire:model="form.tax_label"/>
 
                     <x-adminlte-input type="text" name="tax_number_2" label="Tax number 2:"
-                    placeholder="Tax number 2" fgroup-class="col-md-3" id="tax_number_2" wire:model="tax_number_2"/>
+                    placeholder="Tax number 2" fgroup-class="col-md-3" id="tax_number_2" wire:model="form.tax_number_2"/>
 
                     <x-adminlte-input type="text" name="tax_number_label_2" label="Tax label 2:"
-                    placeholder="Tax label 2" fgroup-class="col-md-3" id="tax_label_2" wire:model="tax_label_2"/>
+                    placeholder="Tax label 2" fgroup-class="col-md-3" id="tax_label_2" wire:model="form.tax_label_2"/>
                 </div>
             </div>
         </div>
@@ -88,13 +97,13 @@
                   </figcaption></p>
                 <div class="row">
                         <x-adminlte-input type="text" name="confirm_password" label="Username" placeholder="Username"
-                        fgroup-class="col-md-4" id="username" wire:model="username"/>
+                        fgroup-class="col-md-4" id="username" wire:model="form.username"/>
 
                         <x-adminlte-input type="password" name="confirm_password" label="Password" placeholder="Password"
-                        fgroup-class="col-md-4" id="password" wire:model="password"/>
+                        fgroup-class="col-md-4" id="password" wire:model="form.password"/>
 
                         <x-adminlte-input type="password" name="confirm_password" label="Confirm Password" placeholder="Password"
-                        fgroup-class="col-md-4" id="confirm_password" wire:model="confirm_password"/>
+                        fgroup-class="col-md-4" id="confirm_password" wire:model="form.confirm_password"/>
 
                 </div>
             </div>
