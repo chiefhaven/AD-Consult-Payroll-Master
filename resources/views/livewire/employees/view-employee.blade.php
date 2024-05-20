6267e5ed9511eb0758d7f3fb724a15dcc1e33166
Chief Haven <chikumbahappy@gmail.com>
2024-05-20 11:18:01 +0200

    Update view-employee.blade.php

--- resources/views/livewire/employees/view-employee.blade.php
+++ resources/views/livewire/employees/view-employee.blade.php
@@ -1,3 +1,28 @@
 <div>
-    <x-adminlte-profile-widget name="User Name"/>
+    <div class="card p-5 employee-profile-header">
+        <div class="row">
+            <div class="col-md-3">
+                <img class="" src="/img/employee-profile.jpg" height="auto" width="100%">
+            </div>
+            <div class="col-md-1"><p>&nbsp;</p></div>
+            <div class="col-md-8">
+                <h2 class="text-white fw-bold">Haven Chikumba</h2>
+                <p class="">
+                    Lorem ipsum: <br>
+                    Dolor sit: <br>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="row m-4">
+        <div class="col-md-8 card p-5">
+            Other information
+        </div>
+        <div class="col-md-1">
+            &nbsp;
+        </div>
+        <div class="col-md-3 card p-5">
+            Upcoming events
+        </div>
+    </div>
 </div>
