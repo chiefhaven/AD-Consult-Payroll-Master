a18fb10b4fa5726e8e9beab3a34f60420b02c5fb
Chief Haven <chikumbahappy@gmail.com>
2024-05-21 23:29:57 +0200

    Create AddClient.php

---
+++ app/Livewire/Clients/AddClient.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Livewire\Clients;
+
+use Livewire\Component;
+use \WW\Countries\Models\Country;
+use App\Livewire\Forms\Client\ClientForm;
+use RealRashid\SweetAlert\Facades\Alert;
+
+class AddClient extends Component
+{
+    public $countries =[];
+
+    public function mount()
+    {
+        $this->countries = Country::all();
+    }
+
+    public ClientForm $form;
+
+    public function save()
+    {
+        $this->form->store();
+
+        session()->flash('status', 'Cieint successfully updated.');
+        return $this->redirect('/clients');
+    }
+
+}
