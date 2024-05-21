2eebaed026412d5cd1bbb0415e419f87102abd19
Chief Haven <chikumbahappy@gmail.com>
2024-05-21 23:28:49 +0200

    Update view-employee.blade.php

--- resources/views/livewire/employees/view-employee.blade.php
+++ resources/views/livewire/employees/view-employee.blade.php
@@ -1,3 +1,46 @@
 <div>
-    <x-adminlte-profile-widget name="User Name"/>
+    <div class="card p-5 employee-profile-header">
+        <div class="row">
+            <div class="col-md-3">
+                <img class="" src="/img/employee-profile.jpg" height="auto" width="100%">
+            </div>
+            <div class="col-md-1"><p>&nbsp;</p></div>
+            <div class="col-md-8">
+                <h2 class="text-white fw-bold">Haven Chikumba</h2>
+                <div class="text-bold">Designation</div>
+                <p class="text-white">
+                    <h3 class="text-white fw-bold">Company name</h3>
+                    <div class="text-white">Address:</div>
+                    <div class="text-white">Phone:</div>
+                    <div class="text-white">Email:</div>
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="row m-4">
+        <div class="col-md-8 card p-5">
+            <div>Employee number: </div>
+            <div>Contract period:</div>
+            <div>Employee on:</div>
+            <div>Age:</div>
+            <div>Gender:</div>
+            <P>
+                <div class="text-bold">Salary structure</div>
+                <div>Gross: </div>
+                <div>Earnings:</div>
+                <div>Deductions:</div>
+                <div>Insurance:</div>
+                <div>Pension:</div>
+                <div>Benefits: </div>
+                <div>Taxes:</div>
+                <div>Net:</div>
+            </P>
+        </div>
+        <div class="col-md-1">
+            &nbsp;
+        </div>
+        <div class="col-md-3 card p-5">
+            Upcoming events
+        </div>
+    </div>
 </div>
