b71dcac6cea7c4e7cfd3986e2235fdcdc9d1c23d
Chief Haven <chikumbahappy@gmail.com>
2024-05-22 23:40:50 +0200

    Revert "Update action-column.blade.php"
    This reverts commit e7051ace164cce64922309d225ca733d1bfce87e.

--- resources/views/tables/action-column.blade.php
+++ resources/views/tables/action-column.blade.php
@@ -3,25 +3,35 @@
       Action
     </button>
     <div class="dropdown-menu p-2" aria-labelledby="dropdownMenuButton">
-        @isset ( $resource )
-            <form wire:submit="viewItem" value="{{ $resource->id }}">
-                <button type="submit" class="dropdown-item mb-1">
-                    <i class="fa fa-solid fa-eye me-2"></i> View
-                </button>
-            </form>
+        @isset ( $employee )
+            <a class="dropdown-item bnt" href="{{ route('view-employee', $employee) }}"><i class="fa fa-solid fa-eye me-2"></i> View</a>
         @endif
 
-        @isset ( $resource )
-            <form wire:submit="editItem" value="{{ $resource->id }}">
-                <button type="submit" class="dropdown-item mb-1">
-                        <i class="fa fa-solid fa-pen me-2"></i> Edit
-                </button>
-            </form>
+        @isset ( $employee )
+            <a class="dropdown-item btn" href="#">
+                <div>
+                    <i class="fa fa-solid fa-pen me-2"></i> Edit
+                </div>
+            </a>
         @endif
 
-        @isset ( $resource )
-            <form wire:submit="deleteItem({{ $resource->id }})" class="bg-danger">
-                <button type="submit" class="dropdown-item bg-danger mb-1">
+        @isset ( $employee )
+            {{--  <form
+                action="#"
+                class="d-inline dropdown-item "
+                method="POST"
+                x-data
+                @submit.prevent="if (confirm('Are you sure you want to delete this user?')) $el.submit()"
+            >
+                @method('DELETE')
+                @csrf
+                <button type="submit" class="btn bg-danger">
+                    <i class="fa fa-solid fa-trash"></i> Delete
+                </button>
+            </form>  --}}
+
+            <form wire:submit="deleteItem" value="{{ $employee }}">
+                <button type="submit" class="btn bg-danger">
                     <i class="fa fa-solid fa-trash"></i> Delete
                 </button>
             </form>
