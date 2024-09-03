0c6e40807f8cbb0909c6891defc38946aa9ff479
Chief Haven <28637676+chiefhaven@users.noreply.github.com>
2024-09-03 14:44:28 +0200

    Update clientView.blade.php

--- resources/views/clients/clientView.blade.php
+++ resources/views/clients/clientView.blade.php
@@ -68,11 +68,21 @@
             </div>
         </div>
         <div class="row m-4">
-            <div class="col-md-8 card p-3">
-                <div class="container d-flex justify-content-end">
-                    <a class="btn btn-primary mb-4" href="{{ route('add-employee', $client) }}">Add Employee</a>
+            <div class="col-md-8 pl-3">
+            <div class="row">
+                <div class="col-md-12 mb-4 card p-3">
+                    <div class="container d-flex justify-content-end">
+                        <a class="btn btn-primary mb-4" href="{{ route('add-employee', $client) }}">Add Employee</a>
+                    </div>
+                    @include('../employees/includes/employeeTable')
                 </div>
-                @include('../employees/includes/employeeTable')
+                <div class="col-md-12 card p-3">
+                    <div class="container d-flex justify-content-end">
+                        <a class="btn btn-primary mb-4" href="{{ route('add-payroll', $client) }}">Add Payroll</a>
+                    </div>
+                    @include('../employees/includes/employeeTable')
+                </div>
+            </div>
             </div>
             <div class="col-md-1">
                 &nbsp;
