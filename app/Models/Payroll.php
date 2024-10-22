93ec87f9fe6280642fdbaee3994ca6c8c3766afc
chiefhaven <chikumbahappy@gmail.com>
2024-10-22 03:01:34 +0200

    Create Payroll.php

---
+++ app/Models/Payroll.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payroll extends Model
+{
+    use HasUuids;
+    use HasFactory;
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(Client::class);
+    }
+
+    public function employees()
+    {
+        return $this->belongsToMany(Employee::class, "payroll_employee")->withPivot('salary', 'pay_period', 'earning_description', 'earning_amount', 'deduction_description', 'deduction_amount', 'payee', 'net_salary', 'total_paid');
+    }
+}
