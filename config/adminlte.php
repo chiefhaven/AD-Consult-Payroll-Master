a9364679ef793fdb710315d3f50abee440a2a4f5
chiefhaven <chikumbahappy@gmail.com>
2025-02-24 16:58:01 +0200

    Update adminlte.php

--- config/adminlte.php
+++ config/adminlte.php
@@ -380,12 +380,6 @@ return [
             ],
         ],
         [
-            'text' => 'Notifications',
-            'url' => 'notifications',
-            'icon' => 'fa fa-bell',
-        ],
-        //['header' => 'account_settings'],
-        [
             'text' => 'Reports',
             'url' => 'reports',
             'icon' => 'fa fas fa-chart-bar',
@@ -449,70 +443,70 @@ return [
 
     'plugins' => [
         'Datatables' => [
-        'active' => true,
-        'files' => [
-            [
-                'type' => 'js',
-                'asset' => false,
-                'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => false,
+                    'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
+                ],
             ],
-            [
-                'type' => 'js',
-                'asset' => false,
-                'location' => '//cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js',
-            ],
-            [
-                'type' => 'css',
-                'asset' => false,
-                'location' => '//cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css',
-            ],
-        ],
         ],
         'DatatablesPlugins' => [
-        'active' => true,
-        'files' => [
-            [
-                'type' => 'js',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/buttons/js/dataTables.buttons.min.js',
-            ],
-            [
-                'type' => 'js',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/buttons/js/buttons.bootstrap4.min.js',
-            ],
-            [
-                'type' => 'js',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/buttons/js/buttons.html5.min.js',
-            ],
-            [
-                'type' => 'js',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/buttons/js/buttons.print.min.js',
-            ],
-            [
-                'type' => 'js',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/jszip/jszip.min.js',
-            ],
-            [
-                'type' => 'js',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/pdfmake/pdfmake.min.js',
-            ],
-            [
-                'type' => 'js',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/pdfmake/vfs_fonts.js',
-            ],
-            [
-                'type' => 'css',
-                'asset' => true,
-                'location' => 'vendor/datatables-plugins/buttons/css/buttons.bootstrap4.min.css',
+            'active' => true,
+            'files' => [
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/buttons/js/dataTables.buttons.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/buttons/js/buttons.bootstrap4.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/buttons/js/buttons.html5.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/buttons/js/buttons.print.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/jszip/jszip.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/pdfmake/pdfmake.min.js',
+                ],
+                [
+                    'type' => 'js',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/pdfmake/vfs_fonts.js',
+                ],
+                [
+                    'type' => 'css',
+                    'asset' => true,
+                    'location' => 'vendor/datatables-plugins/buttons/css/buttons.bootstrap4.min.css',
+                ],
             ],
         ],
-    ],
         'Select2' => [
             'active' => false,
             'files' => [
